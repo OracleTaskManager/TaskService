0bdc22c07b32d52108a22943711165f7d47a7d51
Iñaki GM <inakiglzm@outlook.com>
2025-04-05 18:21:09 -0600

    created .env functionality, user should create a .env file at root level with secrets

--- src/main/java/com/Oracle/TaskService/TaskServiceApplication.java
+++ src/main/java/com/Oracle/TaskService/TaskServiceApplication.java
@@ -1,5 +1,6 @@
 package com.Oracle.TaskService;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TaskServiceApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+		// Set env vars programmatically (optional)
+		System.setProperty("JWT_SECRET_ORACLE", dotenv.get("JWT_SECRET_ORACLE"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 
 		SpringApplication.run(TaskServiceApplication.class, args);
 	}
