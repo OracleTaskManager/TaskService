4a366a1c99d19798e86f88c245f680a697acc173
Iñaki GM <inakiglzm@outlook.com>
2025-05-02 20:21:32 -0600

    Added sout line in Main file

--- src/main/java/com/Oracle/TaskService/TaskServiceApplication.java
+++ src/main/java/com/Oracle/TaskService/TaskServiceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TaskServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("TaskServiceApplication Running");
 		Dotenv dotenv = Dotenv.configure()
 				.ignoreIfMissing() // Optional: avoids crash if .env is missing
 				.load();
